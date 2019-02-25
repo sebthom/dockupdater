a68fa5653c9a89d3b1ebea1fe557aabb5528e4bf
Mathieu Cantin <mcantin@petalmd.com>
2019-02-25 18:07:33 -0500

    Travis: Fix release build

--- scripts/release_deploy.sh
+++ scripts/release_deploy.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
-docker tag docupdater/docupdater:$TRAVIS_BRANCH-amd64 docupdater/docupdater:latest-amd64 docupdater/docupdater:$TRAVIS_TAG-amd64
-docker tag docupdater/docupdater:$TRAVIS_BRANCH-arm docupdater/docupdater:latest-arm docupdater/docupdater:$TRAVIS_TAG-arm
-docker tag docupdater/docupdater:$TRAVIS_BRANCH-arm64 docupdater/docupdater:latest-arm64 docupdater/docupdater:$TRAVIS_TAG-arm64
+docker tag docupdater/docupdater:$TRAVIS_BRANCH-amd64 docupdater/docupdater:$TRAVIS_TAG-amd64
+docker tag docupdater/docupdater:$TRAVIS_BRANCH-arm docupdater/docupdater:$TRAVIS_TAG-arm
+docker tag docupdater/docupdater:$TRAVIS_BRANCH-arm64 docupdater/docupdater:$TRAVIS_TAG-arm64
+
+docker push docupdater/docupdater:$TRAVIS_TAG-amd64
+docker push docupdater/docupdater:$TRAVIS_TAG-arm
+docker push docupdater/docupdater:$TRAVIS_TAG-arm64
 
 docker manifest create docupdater/docupdater:$TRAVIS_TAG docupdater/docupdater:$TRAVIS_TAG-amd64 docupdater/docupdater:$TRAVIS_TAG-arm64 docupdater/docupdater:$TRAVIS_TAG-arm
 docker manifest inspect docupdater/docupdater:$TRAVIS_TAG
 docker manifest push docupdater/docupdater:$TRAVIS_TAG
+
+docker tag docupdater/docupdater:$TRAVIS_BRANCH-amd64 docupdater/docupdater:latest-amd64
+docker tag docupdater/docupdater:$TRAVIS_BRANCH-arm docupdater/docupdater:latest-arm
+docker tag docupdater/docupdater:$TRAVIS_BRANCH-arm64 docupdater/docupdater:latest-arm64
+
+docker push docupdater/docupdater:latest-amd64
+docker push docupdater/docupdater:latest-arm
+docker push docupdater/docupdater:latest-arm64
 
 docker manifest create docupdater/docupdater:latest docupdater/docupdater:latest-amd64 docupdater/docupdater:latest-arm64 docupdater/docupdater:latest-arm
 docker manifest inspect docupdater/docupdater:latest
