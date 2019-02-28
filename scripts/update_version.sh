2c4db44c4bb42d4b3213d65837bae76e9ace689f
Mathieu Cantin <mcantin@petalmd.com>
2019-02-28 13:25:46 -0500

    Travis: Fix tag build

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 if [[ "${TRAVIS_TAG}" != "" ]]; then
-    sed -i -E "s#VERSION = \".*\"#VERSION = \"${TRAVIS_TAG}\"#g"
+    sed -i -E "s#VERSION = \".*\"#VERSION = \"${TRAVIS_TAG}\"#g" docupdater/__init__.py
 fi
