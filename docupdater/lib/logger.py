f748186682443aa8ff03b6939b50fb199075c4a8
Mathieu Cantin <mcantin@petalmd.com>
2019-03-07 10:51:59 -0500

    Remove auth_json from blacklisted keys
    Repo_pass and repo_user already have the same informations

--- docupdater/lib/logger.py
+++ docupdater/lib/logger.py
@@ -6,7 +6,7 @@ class BlacklistFilter(Filter):
     Log filter for blacklisted tokens and passwords
     """
 
-    blacklisted_keys = ['repo_user', 'repo_pass', 'auth_json', 'docker_sockets', 'notifiers']
+    blacklisted_keys = ['repo_user', 'repo_pass', 'docker_sockets', 'notifiers']
 
     def __init__(self, filteredstrings):
         super().__init__()
